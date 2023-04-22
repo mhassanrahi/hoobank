9b40abe12046134db414045d84a6eabf5f7eaf10
mhassanrahi <m.hassan.rahi@gmail.com>
2023-04-23 00:26:07 +0500

    * Navigate user to product section through CTA

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -4,7 +4,8 @@ import { arrowUp } from "../assets";
 
 const GetStarted = () => {
   return (
-    <div
+    <a
+      href="#product"
       className={`${styles.flexCenter} w-[140px]
   h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
     >
@@ -34,7 +35,7 @@ const GetStarted = () => {
           <span className="text-gradient">Started</span>
         </p>
       </div>
-    </div>
+    </a>
   );
 };
 

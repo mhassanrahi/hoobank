79804dadf4111d6c5d2ea1ede52b5bb36f8c770f
mhassanrahi <m.hassan.rahi@gmail.com>
2023-04-22 23:36:19 +0500

    * Added hero section

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from "./Billing";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import GetStarted from "./GetStarted";
+import Stats from "./Stats";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Testimonials from "./Testimonials";
+import FeedbackCard from "./FeedbackCard";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+
+export {
+  Billing,
+  Hero,
+  Navbar,
+  GetStarted,
+  Stats,
+  Business,
+  Button,
+  CardDeal,
+  Testimonials,
+  FeedbackCard,
+  Clients,
+  CTA,
+  Footer,
+};
